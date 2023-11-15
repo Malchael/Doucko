190a147b8b0327275d99fae65d5e8a0afa2af82a
Malchael <150953900+Malchael@users.noreply.github.com>
2023-11-15 20:09:16 +0100

    Doucko

---
+++ script/script.js
@@ -0,0 +1,4 @@
+const obrazek1 = "https://azurlane.netojuu.com/images/thumb/a/a3/Bismarck_Zwei.png/800px-Bismarck_Zwei.png";
+const obrazek2 = "https://azurlane.netojuu.com/images/thumb/b/b6/Unzen.png/922px-Unzen.png";
+const obrazek3 = "https://azurlane.netojuu.com/images/thumb/a/a9/Shinano.png/800px-Shinano.png";
+
